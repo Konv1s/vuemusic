d530cb601615bf40291f365f6982bf0e8e82a16e
Konstantin <kostya_na02>
2022-04-08 12:10:16 +0300

    add validate

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VeeValidatePlugin from './includes/validation';
+import './assets/tailwind.css';
+import './assets/main.css';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VeeValidatePlugin)
+  .mount('#app');
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,16 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    authModalShow: false,
   },
   getters: {
+    // authModalShow: (state) => state.authModalShow,
   },
   mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    },
   },
-  actions: {
-  },
-  modules: {
-  },
+  actions: {},
+  modules: {},
 });
